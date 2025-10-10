4d4316c5a223e37ad21458be672a8516b444fc64
Deepikasel <studiesfor456@gmail.com>
2025-10-10 15:33:04 +0530

    Day 3 training

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("welcome to js")
+a=8;
+console.log(a)
+let object1={age:33};
+let object2 =object1;
+object1.age=22;
+console.log(object2.age)
+console.log(9|5);
+console.log(9^5);
+
+function mul(a,b){
+   return a*b;
+}
+console.log(mul(10,30))
+const sum=(a,b,c)=>{
+   console.log(a+b+c);
+}
+sum(10,20,30);
